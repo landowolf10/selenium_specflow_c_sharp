aabfc07ab338fd09eae1e6c4bb36a493e700abea
Orlando Avila <orlando.avilag@hotmail.com>
2024-02-07 10:35:18 -0600

    Automation framework with Selenium, C# and Specflow.

--- selenium_specflow_c_sharp/Utils/ConstantData.cs
+++ selenium_specflow_c_sharp/Utils/ConstantData.cs
@@ -9,9 +9,9 @@ namespace selenium_specflow_c_sharp.Utils
     public class ConstantData
     {
         public static String chromeDriverPathWindows = "C:\\Users\\orlan\\Documents\\Projects\\Automation\\Selenium\\selenium_specflow_c_sharp\\selenium_specflow_c_sharp\\Utils\\Drivers\\chromedriver.exe";
-        public static String chromeDriverPathLinux = "./src/test/resources/drivers/chromedriver";
+        public static String chromeDriverPathLinux = "./Utils/Drivers/chromedriver";
         public static String geckoDriverPathWindows = "./src/test/resources/drivers/geckodriver.exe";
-        public static String geckoDriverPathLinux = "./src/test/resources/drivers/geckodriver";
+        public static String geckoDriverPathLinux = "./Utils/Drivers/geckodriver";
         public static String URL = "https://www.saucedemo.com";
     }
 }
